fec2791044283a138822be446a670c4517b2486a
sandmandreamcatcher <dr.catcher@ukr.net>
2021-07-17 22:26:50 +0300

    [ShipScriptRefactored]

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,8 +6,7 @@ public class Ship : MonoBehaviour
     [SerializeField] private float _currentHealth;
     [SerializeField] private float _damageValue = 10f;
     [SerializeField] private float _healValue = 10f;
-    private bool _isHealthChanged;
-    
+
     public float MaxHealth => _maxHealth;
 
     public delegate void HealthValueChanged(float currentHealth);
@@ -22,14 +21,11 @@ public class Ship : MonoBehaviour
     {
         _currentHealth += valueToChange;
         NewValueApplied?.Invoke(_currentHealth);
-        _isHealthChanged = false;
     }
     
     private void IncreaseHealth()
     {
-        _isHealthChanged = true;
-
-        if (_isHealthChanged && _currentHealth < _maxHealth)
+        if (_currentHealth < _maxHealth)
             ChangeHealthValue(_healValue);
 
         if (_currentHealth >= _maxHealth)
@@ -38,9 +34,7 @@ public class Ship : MonoBehaviour
 
     private void GetDamage()
     {
-        _isHealthChanged = true;
-
-        if (_isHealthChanged && _currentHealth > 0)
+        if (_currentHealth > 0)
             ChangeHealthValue(-_damageValue);
 
         if (_currentHealth <= 0)
