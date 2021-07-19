1b1d02d75b57f12b3e68b3ee6d36ba37bc3f134c
sandmandreamcatcher <dr.catcher@ukr.net>
2021-07-19 20:20:27 +0300

    [Fixes according to mentor mention]

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -9,33 +9,44 @@ public class Healthbar : MonoBehaviour
     [SerializeField] private Gradient _gradient;
     [SerializeField] private Image _fill;
     [SerializeField] private Ship _ship;
-    
+    private Coroutine _movingCoroutine;
+    private bool _IsSliderMoving;
+
     private void Start()
     {
         _fill.color = _gradient.Evaluate(_slider.normalizedValue);
         _slider.value = _ship.MaxHealth;
         _ship.NewValueApplied += OnNewValueApplied;
     }
-    
+
     private void OnDestroy()
     {
-       _ship.NewValueApplied -= OnNewValueApplied;
+        _ship.NewValueApplied -= OnNewValueApplied;
     }
 
     private void OnNewValueApplied(float valueToChange)
     {
-        StartCoroutine(ChangeProgressBarValue(valueToChange));
+        if (_IsSliderMoving)
+        {
+            StopCoroutine(_movingCoroutine);
+        }
+        
+        _movingCoroutine = StartCoroutine(ChangeProgressBarValue(valueToChange));
     }
 
     private IEnumerator ChangeProgressBarValue(float targetValue)
     {
+        _IsSliderMoving = true;
+
         while (_slider.value != targetValue)
         {
             _slider.value = Mathf.MoveTowards(_slider.value, targetValue, Time.deltaTime * _animationTime);
             yield return null;
         }
+
+        _IsSliderMoving = false;
     }
-    
+
     private void ChangeGradientColor()
     {
         _fill.color = _gradient.Evaluate(_slider.normalizedValue);
